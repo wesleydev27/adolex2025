6a5df65b891e84183c28ca5156af2400009a89d7
Wesley Vaz <72579226+wesleydev27@users.noreply.github.com>
2025-04-22 12:14:36 -0300

    Retirado script da musica e do html

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-    const musica = document.getElementById('musica');
-    musica.play().catch((e) => {
-      console.log("Autoplay bloqueado. Esperando interação do usuário.");
-    });
-  });
